67e1a83108c88ef923b9882bac7426d15688a5c8
Alikhan <alikhanabakarov@gmail.com>
2026-05-24 21:01:01 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -2,9 +2,9 @@
 
 const SUPABASE_URL = 'https://fqxbtojjhpkibixvnbnn.supabase.co';
 
-const SUPABASE_PUBLISHABLE_KEY = '_sb_publishable_KEY';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZxeGJ0b2pqaHBraWJpeHZuYm5uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4NDgyMTQsImV4cCI6MjA5NDQyNDIxNH0.MWHS11ZUbF_okzI385-wiEDCNjQsHFzMl3_K60oA718;
 
-window.supabaseClient = supabase.createClient(
-  SUPABASE_URL,eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZxeGJ0b2pqaHBraWJpeHZuYm5uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4NDgyMTQsImV4cCI6MjA5NDQyNDIxNH0.MWHS11ZUbF_okzI385-wiEDCNjQsHFzMl3_K60oA718
-  SUPABASE_PUBLISHABLE_KEY
+window.supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
 );
