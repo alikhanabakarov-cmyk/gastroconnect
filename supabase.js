a9523fff36fcd4ca5e6e5b0ae33a13c1e77628f6
Alikhan <alikhanabakarov@gmail.com>
2026-05-24 20:58:50 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -5,6 +5,6 @@ const SUPABASE_URL = 'https://fqxbtojjhpkibixvnbnn.supabase.co';
 const SUPABASE_PUBLISHABLE_KEY = '_sb_publishable_KEY';
 
 window.supabaseClient = supabase.createClient(
-  SUPABASE_URL,sb_publishable_0F-CDySnOiJYUdAr8khcJA_QiZc6J2y
+  SUPABASE_URL,eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZxeGJ0b2pqaHBraWJpeHZuYm5uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4NDgyMTQsImV4cCI6MjA5NDQyNDIxNH0.MWHS11ZUbF_okzI385-wiEDCNjQsHFzMl3_K60oA718
   SUPABASE_PUBLISHABLE_KEY
 );
